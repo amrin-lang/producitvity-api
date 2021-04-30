09a9da75c3d7189b7697d8080018fe4b6a11aedc
amrin-lang <aana0006@student.monash.edu>
2021-04-30 21:32:16 +1000

    Remove Task array from User model

--- models/User.js
+++ models/User.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose')
 //type, required, minlength, default, maxlength, enum
 const userSchema = mongoose.Schema({
-  username: {
-    type: String,
+  user_id: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
     required: [true, "Username is required"],
-    unique: true
   },
 
   password:{
@@ -16,41 +16,6 @@ const userSchema = mongoose.Schema({
     type: String 
   },
 
-  tasks:[{
-    task_id:{
-      type: String,
-      required:[true,"task id needed"]
-    },
-
-    title:{
-      type: String,
-      required: [true,"title of task required"]
-    },
-
-    description:{
-      type: String
-    },
-
-    target_date:{
-      type: Date,
-      required: [true, "User need to enter target date"]      
-    },
-
-    reminder:{
-      type: Date,
-      required:[true, "user needs to enter reminder time"]      
-    },
-
-    list_type:{
-      type: String,
-      enum: ["TO-DO", "IT'S-DONE"],
-      default: "TO-DO"
-    },
-
-    timestamp_marked_complete:{
-      type: Date
-    }
-  }]
 
 })
 
